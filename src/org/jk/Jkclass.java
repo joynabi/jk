54d8df72e8632a1fdcd383a79479612863adaf76
joynabi <joynabiya.arava398@gmail.com>
2021-10-27 10:57:46 +0530

    task completed

---
+++ src/org/jk/Jkclass.java
@@ -0,0 +1,13 @@
+package org.jk;
+
+public class Jkclass {
+
+	public void s1() {
+		System.out.println("s1");
+
+	}
+	public static void main(String[] args) {
+		Jkclass jk = new Jkclass();
+		jk.s1();
+	}
+}
